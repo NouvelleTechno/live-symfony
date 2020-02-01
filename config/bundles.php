1a8ba811a3d86664aeb037238f16632e0c876d30
Benoit Gambier <contact@nouvelle-techno.fr>
2020-02-01 13:30:29 +0100

    Fin Liip Imagine

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     FM\ElfinderBundle\FMElfinderBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
