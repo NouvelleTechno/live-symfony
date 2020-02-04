486d90c4cfaef76f6782525678bc50eacb12e62a
Benoit Gambier <contact@nouvelle-techno.fr>
2020-02-04 14:40:03 +0100

    Envoi email

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function index(Request $request, \Swift_Mailer $mailer)
+    {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $contact = $form->getData();
+
+            // Ici nous enverrons le mail
+            $message = (new \Swift_Message('Nouveau Contact'))
+                // On attribue l'expéditeur
+                ->setFrom($contact['email'])
+
+                // On attribue le destinataire
+                ->setTo('votre@adresse.fr')
+
+                // On crée le message avec la vue Twig
+                ->setBody(
+                    $this->renderView(
+                        'emails/contact.html.twig', compact('contact')
+                    ),
+                    'text/html'
+                )
+            ;
+
+            // On envoie le message
+            $mailer->send($message);
+
+            $this->addFlash('message', 'Le message a bien été envoyé');
+            return $this->redirectToRoute('accueil');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'contactForm' => $form->createView()
+        ]);
+    }
+}
