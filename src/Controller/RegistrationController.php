44521df2eb16f77baf8b66569945c8ffbc66dfb4
Benoit Gambier <contact@nouvelle-techno.fr>
2020-02-08 20:44:13 +0100

    Fin live

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\RegistrationFormType;
+use App\Repository\UsersRepository;
 use App\Security\UsersAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,7 +18,7 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/inscription", name="app_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UsersAuthenticator $authenticator): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, UsersAuthenticator $authenticator, \Swift_Mailer $mailer): Response
     {
         $user = new Users();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -32,11 +33,31 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            // On génère le token d'activation
+            $user->setActivationToken(md5(uniqid()));
+
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
 
             // do anything else you need here, like send an email
+            // On crée le message
+            $message = (new \Swift_Message('Activation de votre compte'))
+                // On attribue l'expéditeur
+                ->setFrom('votre@adresse.fr')
+                // On attribue le destinataire
+                ->setTo($user->getEmail())
+                // On crée le contenu
+                ->setBody(
+                    $this->renderView(
+                        'emails/activation.html.twig', ['token' => $user->getActivationToken()]
+                    ),
+                    'text/html'
+                )
+            ;
+
+            // On envoie l'e-mail
+            $mailer->send($message);
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
@@ -50,4 +71,32 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
         ]);
     }
+
+    /**
+     * @Route("/activation/{token}", name="activation")
+     */
+    public function activation($token, UsersRepository $usersRepo){
+        // On vérifie si un utilisateur a ce token
+        $user = $usersRepo->findOneBy(['activation_token' => $token]);
+
+        // Si aucun utilisateur n'existe avec ce token
+        if(!$user){
+            // Erreur 404
+            throw $this->createNotFoundException('Cet utilisateur n\'existe pas');
+        }
+
+        // On supprime le token
+        $user->setActivationToken(null);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        // On envoie un message flash
+        $this->addFlash('message', 'Vous avez bien activé votre compte');
+
+        // On retoure à l'accueil
+        return $this->redirectToRoute('accueil');
+    }
+
+    
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use App\Form\ResetPassType;
+use App\Repository\UsersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -33,4 +40,103 @@ class SecurityController extends AbstractController
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
+
+    
+    /**
+     * @Route("/oubli-pass", name="forgotten_password")
+     */
+    public function forgottenPass(Request $request, UsersRepository $usersRepo, \Swift_Mailer $mailer, TokenGeneratorInterface $tokenGenerator){
+        // On crée le formulaire
+        $form = $this->createForm(ResetPassType::class);
+
+        // On traite le formulaire
+        $form->handleRequest($request);
+
+        // Si le formulaire est valide
+        if($form->isSubmitted() && $form->isValid()){
+            // On récupère les données
+            $donnees = $form->getData();
+
+            // On cherche si un utilisateur a cet email
+            $user = $usersRepo->findOneByEmail($donnees['email']);
+
+            // Si l'utilisateur n'existe pas
+            if(!$user){
+                // On envoie un message flash
+                $this->addFlash('danger', 'Cette adresse n\'existe pas');
+
+                return $this->redirectToRoute('app_login');
+            }
+
+            // On génère un token
+            $token = $tokenGenerator->generateToken();
+
+            try{
+                $user->setResetToken($token);
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
+            }catch(\Exception $e){
+                $this->addFlash('warning', 'Une erreur est survenue : '. $e->getMessage());
+                return $this->redirectToRoute('app_login');
+            }
+
+            // On génère l'URL de réinitialisation de mot de passe
+            $url = $this->generateUrl('app_reset_password', ['token' => $token],UrlGeneratorInterface::ABSOLUTE_URL);
+
+            // On envoie le message
+            $message = (new \Swift_Message('Mot de passe oublié'))
+            ->setFrom('votre@adresse.fr')
+            ->setTo($user->getEmail())
+            ->setBody(
+                "<p>Bonjour,</p><p>Une demande de réinitialisation de mot de passe a été effectuée pour le site Nouvelle-Techno.fr. Veuillez cliquer sur le lien suivant : " . $url .'</p>',
+                'text/html'
+                )
+            ;
+
+            // On envoie l'e-mail
+            $mailer->send($message);
+
+            // On crée le message flash
+            $this->addFlash('message', 'Un e-mail de réinitialisation de mot de passe vous a été envoyé');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        // On envoie vers la page de demande de l'e-mail
+        return $this->render('security/forgotten_password.html.twig', ['emailForm' => $form->createView()]);
+    }
+    
+    /**
+     * @Route("/reset-pass/{token}", name="app_reset_password")
+     */
+    public function resetPassword($token, Request $request, UserPasswordEncoderInterface $passwordEncoder){
+        // On cherche l'utilisateur avec le token fourni
+        $user = $this->getDoctrine()->getRepository(Users::class)->findOneBy(['reset_token' => $token]);
+
+        if(!$user){
+            $this->addFlash('danger', 'Token inconnu');
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Si le formulaire est envoyé en méthode POST
+        if($request->isMethod('POST')){
+            // On supprime le token
+            $user->setResetToken(null);
+
+            // On chiffre le mot de passe
+            $user->setPassword($passwordEncoder->encodePassword($user, $request->request->get('password')));
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('message', 'Mot de passe modifié avec succès');
+
+            return $this->redirectToRoute('app_login');
+        }else{
+            return $this->render('security/reset_password.html.twig', ['token' => $token]);
+        }
+
+    }
+
 }
